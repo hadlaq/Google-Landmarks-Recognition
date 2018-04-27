09681c22461629e00077212e58931b5e18693e40
Omar Alhadlaq <hadlaq@cs.washington.edu>
2018-04-27 15:29:10 -0700

    script to unzip

--- tools/download_data.py
+++ tools/download_data.py
@@ -2,66 +2,31 @@
 
 # Edited script from Kaggle
 
-import sys, os, multiprocessing, urllib2, csv
-from PIL import Image
-from StringIO import StringIO
+
+import os.path
+import zipfile
 
 
-def ParseData(data_file):
-  csvfile = open(data_file, 'r')
-  csvreader = csv.reader(csvfile)
-  key_url_list = [line[:2] for line in csvreader]
-  return key_url_list[1:]  # Chop off header
+def unzip_files(data_folder):
+    files = ["train.csv", "test.csv", "sample_submission.csv"]
+    for f in files:
+        unzip(data_folder, f)
 
 
-def DownloadImage(key_url):
-  out_dir = sys.argv[2]
-  (key, url) = key_url
-  filename = os.path.join(out_dir, '%s.jpg' % key)
+def unzip(data_folder, fname):
+    if os.path.isfile(fname):
+        print(fname + " already unzipped.")
+        return
 
-  if os.path.exists(filename):
-    print('Image %s already exists. Skipping download.' % filename)
-    return
-
-  try:
-    response = urllib2.urlopen(url)
-    image_data = response.read()
-  except:
-    print('Warning: Could not download image %s from %s' % (key, url))
-    return
-
-  try:
-    pil_image = Image.open(StringIO(image_data))
-  except:
-    print('Warning: Failed to parse image %s' % key)
-    return
-
-  try:
-    pil_image_rgb = pil_image.convert('RGB')
-  except:
-    print('Warning: Failed to convert image %s to RGB' % key)
-    return
-
-  try:
-    pil_image_rgb.save(filename, format='JPEG', quality=90)
-  except:
-    print('Warning: Failed to save image %s' % filename)
-    return
+    zip_ref = zipfile.ZipFile(data_folder + fname + '.zip', 'r')
+    zip_ref.extractall(data_folder)
+    zip_ref.close()
+    print(fname + " unzipped.")
 
 
-def Run():
-  if len(sys.argv) != 3:
-    print('Syntax: %s <data_file.csv> <output_dir/>' % sys.argv[0])
-    sys.exit(0)
-  (data_file, out_dir) = sys.argv[1:]
-
-  if not os.path.exists(out_dir):
-    os.mkdir(out_dir)
-
-  key_url_list = ParseData(data_file)
-  pool = multiprocessing.Pool(processes=50)
-  pool.map(DownloadImage, key_url_list)
-
+def main():
+    data_folder = '../data/metadata/'
+    unzip_files(data_folder)
 
 if __name__ == '__main__':
-  Run()
+    main()
